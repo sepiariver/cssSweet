fbfd214fcab57105b1cad51e9f9fd3696f5be114
sepiariver <info@sepiariver.com>
2015-01-02 22:33:37 -0800

    bumped to 1.2.0-pl

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -23,8 +23,8 @@ if (!defined('MOREPROVIDER_BUILD')) {
     /* define version */
     define('PKG_NAME','cssSweet');
     define('PKG_NAME_LOWER',strtolower(PKG_NAME));
-    define('PKG_VERSION','1.1.0');
-    define('PKG_RELEASE','rc1');
+    define('PKG_VERSION','1.2.0');
+    define('PKG_RELEASE','pl');
 
     /* load modx */
     require_once dirname(dirname(__FILE__)) . '/config.core.php';
