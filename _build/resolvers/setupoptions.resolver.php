99b3a8df15c9f4a9ef9dd108c029214832435b62
sepiariver <info@sepiariver.com>
2015-05-01 17:39:29 -0700

    first try at resolver

--- _build/resolvers/setupoptions.resolver.php
+++ _build/resolvers/setupoptions.resolver.php
@@ -10,10 +10,14 @@ $success= false;
 switch ($options[xPDOTransport::PACKAGE_ACTION]) {
     case xPDOTransport::ACTION_INSTALL:
     case xPDOTransport::ACTION_UPGRADE:
-        if ($options['confirm_upgrade_breaking_changes']) {
+        $modx->log(modX::LOG_LEVEL_ERROR,'Confirmation not selected. Installation aborted.');//test
+        $success= false;//test
+        die();//test
+        if (isset($options['confirm_upgrade_breaking_changes']) && !empty($options['confirm_upgrade_breaking_changes'])) {
             $success= true;
         } else {
-            $success= false;
+            $modx->log(modX::LOG_LEVEL_WARN,'Confirmation not selected. Installation aborted.');
+            $success= false;  
         }
         break;
     case xPDOTransport::ACTION_UNINSTALL:
