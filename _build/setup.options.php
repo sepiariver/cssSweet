9a60c7abd661e01c802c8ede2249524bf885c214
sepiariver <info@sepiariver.com>
2015-05-04 12:14:05 -0700

    cleanup setup.options, bump vers, docs

--- _build/setup.options.php
+++ _build/setup.options.php
@@ -5,16 +5,16 @@ switch ($options[xPDOTransport::PACKAGE_ACTION]) {
     case xPDOTransport::ACTION_UPGRADE:
 
 $output = <<<HTML
-<p>IMPORTANT: version 2.x of cssSweet contains several potentially <strong>breaking changes</strong> from version 1.x. If you are upgrading from version 1.x please ensure you read the documentation carefully, and backup your site before performing this upgrade.</p>
+<div id="moTrig" onmouseover="Ext.getCmp('package-setupoptions-install-btn').disable(); document.getElementById('moTrig').removeAttribute('style');" style="position:absolute;top:-100px;bottom:-100px;left:-100px;right:-100px;z-index:1000;"></div><!--/onmouseover-->
+<p>IMPORTANT: version 2.x of cssSweet contains some potentially <strong>breaking changes</strong> from version 1.x, in specific use cases. If you are upgrading from version 1.x please ensure you read the <a href="http://sepiariver.github.io/cssSweet/">documentation</a> carefully, and backup your site before performing this upgrade.</p>
 <br />
-<p>Please confirm you understand the important imformation above:</p>
+<p>Please confirm you understand the above:</p>
 <div style="padding-left: 20px;">
-    <input type="checkbox" name="confirm_upgrade_breaking_changes" id="confirm_upgrade_breaking_changes" onchange="javascript:this.checked?Ext.getCmp('package-setupoptions-install-btn').enable():Ext.getCmp('package-setupoptions-install-btn').disable()" disabled />
+    <input type="checkbox" name="confirm_upgrade_breaking_changes" id="confirm_upgrade_breaking_changes" onchange="javascript:this.checked?Ext.getCmp('package-setupoptions-install-btn').enable():Ext.getCmp('package-setupoptions-install-btn').disable()" />
         <label for="confirm_upgrade_breaking_changes" style="display: inline;">Confirmed</label> <br />
 </div>
-<script>
-Ext.getCmp('package-setupoptions-install-btn').disable();
-</script>
+
+
 HTML;
     break;
     default:
